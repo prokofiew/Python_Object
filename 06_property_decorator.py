3f20ea97ee6e9d0de884062ef86b72bb59db5611
prokofiew <filipmaciborski@gmail.com>
2023-11-06 23:02:55 +0100

    learning properties, decorators

---
+++ 06_property_decorator.py
@@ -0,0 +1,53 @@
+class Employee:
+
+    def __init__(self, first, last):
+        self.first = first.strip().title()
+        self.last = last.strip().title()
+        self.email = (self.first + '.' + self.last + '@company.com').lower()
+
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+
+class Employee1:
+
+    def __init__(self, first, last):
+        self.first = first.strip().title()
+        self.last = last.strip().title()
+
+# when property, don't need () to call method and there is no need to change code where email attribute is called
+    @property
+    def email(self):
+        return f'{self.first}.{self.last}@company.com'.lower().strip()
+
+    @property
+    def fullname(self):
+        return f'{self.first} {self.last}'.strip().title()
+
+
+print('----------------class without property----------------------------')
+emp_1 = Employee('   john   ', 'smith   ')
+print(emp_1.first)
+# email without () because it's an attribute placed in constructor
+print(emp_1.email)
+# if it's a regular method, have to add () in code everywhere where email is called
+print(emp_1.fullname())
+emp_1.first = 'Jim'
+print('name change: ' + emp_1.first)
+# email grabs the current first name
+print('-> ' + emp_1.email)
+# print(emp_1.email) for email attribute in constructor
+print(emp_1.fullname())
+
+
+print('----------------class with property----------------------------')
+emp_2 = Employee1('    maria   ', ' something')
+print('name: ' + emp_2.first)
+# email grabs the current first name
+print(emp_2.email)
+# print(emp_1.email) for email attribute in constructor
+print('fullname: ' + emp_2.fullname)
+emp_2.first = 'edward'
+print('name change: ' + emp_2.first)
+print(emp_2.email)
+print(emp_2.fullname)
